a74dabe44afb6153472d75e2ec38af4bfb33dc32
hcnewsom@gmail.com <hcnewsom@dyn-209-2-209-224.dyn.columbia.edu>
2012-02-02 17:07:27 -0500

    removing extraneous files

--- Resources/views/more.js
+++ Resources/views/more.js
@@ -6,36 +6,12 @@ var data = [
 	{title:'YouTube', hasChild:true, url:'http://www.youtube.com/user/columbiauniversity'},
 	{title:'Facebook', hasChild:true, url:'http://www.facebook.com/ColumbiaAlumniAssoc'},
 	{title:'Twitter', hasChild:true, url:'http://twitter.com/#!/Columbia'},
-	/*{title:'Local URL', hasChild:true, url:'local_webview.html'},
-	{title:'XHR to Filesystem', hasChild:true},	
-	{title:'Image URL', hasChild:true, url:'http://www.appcelerator.com/wp-content/uploads/2010/01/TABWAVE_graph1.png'},
-	{title:'Inline HTML', hasChild:true, innerHTML:'<html><body>Hello from inline HTML.</body></html>'},
-	{title:'Inline HTML w/ Trans Bg', hasChild:true, innerHTML:'<html><body><div style="color:white;">Hello from inline HTML. You should see white text and black background</div></body></html>', bgcolor:'black'},
-	{title:'Inline HTML w/ Color Bg', hasChild:true, innerHTML:'<html><body><div style="color:red;">Hello from inline HTML. You should see red text and yellow background</div></body></html>', bgcolor:'yellow'},
-	{title:'Basic Auth', hasChild:true, url: 'http://irisresearch.library.cornell.edu/control/authBasic/authTest/', username: 'test', password: 'this'},
-	{title:'Logging and Unicode', hasChild:true, url:'webview_logging.html'}*/
+
 ];
 
 // add iphone specific tests
 if (Titanium.Platform.name == 'iPhone OS')
 {
-	/*data.push({title:'Local Eval', hasChild:true, url:'local_webview.html', evaljs:true});
-	data.push({title:'Local HTML', hasChild:true, url:'local_webview.html', evalhtml:true});
-	data.push({title:'Inline HTML w/ Border', hasChild:true, innerHTML:'<html><body><div>Hello from inline HTML. You should see red border</div></body></html>', border: true});
-	data.push({title:'PDF URL', hasChild:true, url:'http://www.appcelerator.com/assets/The_iPad_App_Wave.pdf'});
-	data.push({title:'SVG URL', hasChild:true, url:'http://upload.wikimedia.org/wikipedia/commons/5/55/1st_Cavalry_Division_-_Shoulder_Sleeve_Insignia.svg'});
-	data.push({title:'Local image', hasChild:true, url:'../images/cloud.png'});
-	data.push({title:'Local SVG', hasChild:true, url:'../images/insignia.svg'});
-	data.push({title:'Local Pinch/Zoom', hasChild:true, url:'local_webview_pinchzoom.html', scale:true});
-	data.push({title:'Webview controls', hasChild:true, url:'http://www.google.com', controls:true});*
-	// The result for this is going to be centered, because that's where layout puts it.
-	// But users can make sure that embedded webviews are anchored in the usual way.
-	data.push({title:'Auto Size', auto:true, hasChild:true, innerHTML:'<html><body style="height:200px;width:100px;border:1px solid #ccc;padding:10px">200 px height, 100 px width.</body></html>'});
-	data.push({title:'Partial Auto', hasChild:true, partial:true, innerHTML:'<html><body><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div><hr/><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></body></html>'});
-	
-	// this should work on android too, right?
-	data.push({title:'HTML5 Video', auto:true, hasChild:true, url:'html5video.html'});
-	*/
 	// can't test youtube in simulator
 	if (Titanium.Platform.model != 'Simulator')
 	{
@@ -68,11 +44,7 @@ tableview.addEventListener('click', function(e)
 	{
 		webview = Ti.UI.createWebView();
 	}
-	//	webview.addEventListener('singletap', function(e)
-	//	{
-	//		alert('singletap');
-	//	});
-	//	handle xhr to filesystem case first
+
 	if (e.index == 8)
 	{
 		w.add(webview);
