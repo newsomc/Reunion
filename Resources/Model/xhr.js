173e817447435df034d14ca0f7d9752d003f08ca
hcnewsom@gmail.com <hcnewsom@Clint-Newsoms-MacBook-Pro.local>
2012-02-28 22:25:31 -0500

    adding network checks

--- Resources/Model/xhr.js
+++ Resources/Model/xhr.js
@@ -5,27 +5,34 @@
  */
 
 function getReunionData(url_params, _success, _error) {
- 
-   var xhr = Ti.Network.createHTTPClient(); // returns an instance of HTTPClient
- 
-   xhr.onload = function(e) {
-       if(typeof _success == "function"){
-          return _success(this.responseText);
-       } 
-   };
- 
-   xhr.onerror = function(e) {
-       //Titanium.API.log('INFO', e.message);
-       if (_error) {
-          _error(e);
-          alert('There was an error with this request. Please try again later.');
-       } 
-   };
- 
-   xhr.setTimeout(15000); 
- 
-   xhr.open('GET','https://ccit.college.columbia.edu/reunion-base/service' + url_params, true);
-   //xhr.open('GET','https://chameleon.college.columbia.edu/reunion_base/service' + url_params, true);
-   Ti.API.info('https://ccit.college.columbia.edu/reunion-base/service' + url_params);
-   xhr.send(); 
+
+	var xhr = Ti.Network.createHTTPClient();
+	// returns an instance of HTTPClient
+
+	if(Ti.Network.online) {
+		xhr.onload = function(e) {
+			if( typeof _success == "function") {
+				return _success(this.responseText);
+			}
+		};
+
+		xhr.onerror = function(e) {
+			//Titanium.API.log('INFO', e.message);
+			if(_error) {
+				_error(e);
+				alert('There was an error with this request. Please try again later.');
+			}
+		};
+
+		xhr.setTimeout(15000);
+
+		xhr.open('GET', 'https://ccit.college.columbia.edu/reunion-base/service' + url_params, true);
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		//xhr.open('GET','https://chameleon.college.columbia.edu/reunion_base/service' + url_params, true);
+		//Ti.API.info('https://ccit.college.columbia.edu/reunion-base/service' + url_params);
+		xhr.send();
+	} else {
+		alert('Your device is not online.');
+		
+	}
 }
