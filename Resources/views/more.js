12a7945e787dddafe193f8828a65689c3416c3b4
hcnewsom@gmail.com <hcnewsom@Clint-Newsoms-MacBook-Pro.local>
2012-02-28 22:27:21 -0500

    clean up

--- Resources/views/more.js
+++ Resources/views/more.js
@@ -1,25 +1,23 @@
 var win = Titanium.UI.currentWindow;
 Ti.include(Titanium.Filesystem.resourcesDirectory + 'views/setupWindow.js');
-setUpWindow('More');
 
-// create table view data object
-var data = [
+win.addEventListener('focus', function() {
+  setUpWindow('More');
+
+  // create table view data object
+  var data = [
 	{title:'Columbia College Alumni', hasChild:true, url:'http://www.college.columbia.edu/alumni'},
 	{title:'Facebook', hasChild:true, url:'http://www.facebook.com/ColumbiaCollege1754'},
 	{title:'CCAA Twitter', hasChild:true, url:'http://twitter.com/#!/Columbia_CCAA'},
+  ];
 
-];
+  // create table view
+  var tableview = Titanium.UI.createTableView({
+    data:data
+  });
 
-// create table view
-var tableview = Titanium.UI.createTableView({
-	
-	data:data
-
-});
-
-tableview.addEventListener('click', function(e) {
-	
-	var rowdata = e.rowData;
+  tableview.addEventListener('click', function(e) {
+    var rowdata = e.rowData;
 	
 	var link_win = Ti.UI.createWindow({
 		title: rowdata.title,
@@ -35,6 +33,8 @@ tableview.addEventListener('click', function(e) {
 	link_win.add(webview);
 	
 	win.tab.open(link_win);	
-});
+  });
 
-win.add(tableview);
+  win.add(tableview);
+
+});
